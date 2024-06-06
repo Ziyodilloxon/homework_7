89b376545e6a2e5c5ed7b38130ad8c2b3799f344
ziyodilloxon <ziyodilloxon8@gmail.com>
2024-06-07 03:38:24 +0500

    7 darsning uy ishi

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+// styles
+import "./App.css";
+// layout
+import MainLayout from "./layout/MainLayout";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+
+// react router dom imports
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/product/:id",
+          element: <Product />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={routes} />;
+}
+
+export default App;
